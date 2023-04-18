9e0c500e96ac3cba98a2305314e866482c3b17ff
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2023-04-18 16:07:10 -0300

    🎉 first commit

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tw-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent {
+
+}
